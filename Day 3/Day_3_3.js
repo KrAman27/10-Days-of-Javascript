a461805f18fbfdb172880bdb034db745557e7a49
Aman Kumar <amankr2702@gmail.com>
2020-12-24 21:49:25 +0530

    Loops

---
+++ Day 3/Day_3_3.js
@@ -0,0 +1,13 @@
+function vowelsAndConsonants(s) {
+    for(var i=0;i<s.length;i++){
+        if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
+            console.log(s[i]);
+        }
+    }
+    
+    for(var i=0;i<s.length;i++){
+        if(s[i]!='a' && s[i]!='e' && s[i]!='i' && s[i]!='o' && s[i]!='u'){
+            console.log(s[i]);
+        }
+    }
+}
